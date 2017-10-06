0d41368871aa92f832028fa4652c0d4bb8271823
seraphinush <30970630+seraphinush@users.noreply.github.com>
2017-10-06 15:59:10 +0900

    Update index.js
    - Personalized code aesthetics
    - Removed config.json
    - Fixed auto-interval

--- index.js
+++ index.js
@@ -1,101 +1,108 @@
-const config = require('./config.json');
+// OPCODE REQUIRED : 
+// - C_PLAYER_LOCATION
+// - C_TRY_LOOT_DROPITEM
+// - S_DESPAWN_DROPITEM
+// - S_LOGIN
+// - S_LOAD_TOPO
+// - S_MOUNT_VEHICLE
+// - S_SPAWN_DROPITEM
+// - S_UNMOUNT_VEHICLE
 
-module.exports = function Loot(dispatch) {
+const blacklist = [
+    8000, // Rejuvenation Mote
+    8001, // HP Recovery Mote
+    8002, // MP Replenishment Mote
+    8005, // Healing Mote
+    8018, // Arun's Vitae XI Mote
+    8023, // Arun's Tear Mote
+    8025, // Keening Dawn Mote
+    139113, // 행운의 상자 (kTERA)
+    166718, // 행운의 상자 (kTERA)
+    213026, // 행운의 상자 (kTERA)
+]
 
-    let auto = config.modes.auto || false,
-        enabled = config.modes.easy || true,
-        lootInterval = auto ? setInterval(tryLootAll, 250) : null,
-        location;
+module.exports = function AutoLoot(dispatch) {
 
-    let blacklist = config.blacklist || [8000, 8001, 8002, 8005, 8018, 8025, 8023];
+    let auto = true,
+        enable = true,
+        location,
+        mounted
 
-    let loot = {};
+    let loot = {}
 
-    let commands = {
-        auto: {
-            alias: ['auto', 'autoloot', 'toggle'],
-            run: function() {
-                auto = !auto;
-                message(`Autoloot mode toggled: ${auto}`);
-                if(auto){
-					lootInterval = setInterval(tryLootAll, 250);
-				}
-				else
-					clearInterval(lootInterval);
+    var loop = null
+
+    // command
+    try {
+        const Command = require('command')
+        const command = Command(dispatch)
+        command.add('loot', (arg) => {
+            // toggle
+            if (arg === undefined) {
+                enable = !enable
+                send(`[auto-loot] : Ranged ${enable ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'}.`)
+            // auto
+            } else if (arg == 'auto') {
+                auto = !auto
+                setup()
+                send(`[auto-loot] : Auto ${auto ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'}.`)
+            } else if (arg) {
+                send(`[auto-loot] : <font color="#FF0000">Invalid argument.</font>`)
             }
-        },
-        enable: {
-            alias: ['enable', 'on'],
-            run: function() {
-                enabled = true;
-                message('Easy looting is enabled.');
-            }
-        },
-        disable: {
-            alias: ['disable', 'off'],
-            run: function() {
-                enabled = false;
-                message('Easy looting is disabled.');
-            }
+        })
+        function send(msg) {
+            command.message(`<font color="#FFFFFF">` + msg + `</font>`)
         }
-    }
+	} catch (e) {
+		console.log(`[ERROR] -- auto-loot module --`)
+	}
+    
+    // code
+    dispatch.hook('S_LOGIN', (event) => { setup() })
+    dispatch.hook('S_LOAD_TOPO', 1, (event) => { 
+        loot = {},
+        mounted = false
+    })
+    dispatch.hook('C_PLAYER_LOCATION', 1, (event) => { location = event })
 
-    dispatch.hook('C_CHAT', 1, (event) => {
-        if(!event.message.includes('!loot'))
-            return;
-
-        let command = event.message.replace(/<\/?[^<>]*>/gi, '').split(' ');
-
-        if(command.length > 1) {
-            for(let cmd in commands) {
-                if(commands[cmd].alias.indexOf(command[1].toString()) > -1)
-                    commands[cmd].run();
-            }
-        }
-
-        return false;
-    });
-
-    dispatch.hook('S_LOAD_TOPO', 1, (event) => {
-        loot = {};
-    });
-
-    dispatch.hook('C_PLAYER_LOCATION', 1, (event) => {
-        location = event;
-    });
+    dispatch.hook('S_MOUNT_VEHICLE', (event) => { mounted = true })
+    dispatch.hook('S_UNMOUNT_VEHICLE', (event) => { mounted = false })
 
     dispatch.hook('S_SPAWN_DROPITEM', 1, (event) => {
-        if(!(blacklist.indexOf(event.item) > -1)) loot[event.id.toString()] = event;
-    }); 
-
-    dispatch.hook('C_TRY_LOOT_DROPITEM', 1, (event) => {
-        if(enabled) tryLootAll();      
-    });
+        if (!(blacklist.indexOf(event.item) > -1)) loot[event.id.toString()] = event
+    }) 
     
     dispatch.hook('S_DESPAWN_DROPITEM', 1, (event) => {
-        if(event.id.toString() in loot) delete loot[event.id.toString()];    
-    });
+        if (event.id.toString() in loot) delete loot[event.id.toString()]
+    })
 
-    function tryLootAll() {
-        for(let item in loot) {
-            if(location)
-                if(Math.abs(loot[item].x - location.x1) < 120 && Math.abs(loot[item].y - location.y1) < 120)
+    // credit : Alejandro Ojeda (Github : alexoj)
+    dispatch.hook('S_SYSTEM_MESSAGE_LOOT_ITEM', (event) => {
+        if (event.message === '@41') return false
+    })
+
+    // for when auto is disabled
+    dispatch.hook('C_TRY_LOOT_DROPITEM', 1, (event) => { lootAll() })
+
+    function lootAll() {
+        if (!enable) return
+        if (mounted) return
+        for (let item in loot) {
+            if (location) {
+                if (Math.abs(loot[item].x - location.x1) < 120 && Math.abs(loot[item].y - location.y1) < 120) {
                     dispatch.toServer('C_TRY_LOOT_DROPITEM', 1, {
                         id: loot[item].id
-                    });
+                    })
+                }
+            }
         }
     }
 
-    function message(msg) {
-        dispatch.toClient('S_CHAT', 1, {
-            channel: 24,
-            authorID: 0,
-            unk1: 0,
-            gm: 0,
-            unk2: 0,
-            authorName: '',
-            message: ' (autoloot) ' + msg
-        });
+    // helper
+    function setup() {
+        clearInterval(loop)
+        loop = auto ? setInterval(lootAll, 250) : null
     }
 
 }
+
