84d20997f6138f59089b09d94be6e0b55518e103
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-02-25 10:46:09 +0900

    1.35 r:01

--- index.js
+++ index.js
@@ -9,26 +9,22 @@
 // - S_SYSTEM_MESSAGE
 // - S_UNMOUNT_VEHICLE
 
-// Version 1.35 r:00
+// Version 1.35 r:01
 
 const blacklist = [
     7214, // Scroll of Resurrection
     8000, // Rejuvenation Mote
     8001, // HP Recovery Mote
     8002, // MP Replenishment Mote
+    8003, // Spirited MP Replenishment Mote
+    8004, // Strong Resistance Mote
     8005, // Healing Mote
-    8018, // Arun's Vitae XI Mote
+    8008, 8009, 8010, 8011, 8012, 8013, 8014, 8015, 8016, 8017, 8018, 8019, 8020, 8021, 8022, // Arun's Vitae I-XV Mote
     8023, // Arun's Tear Mote
     8025, // Keening Dawn Mote
-    169886,
-    169887,
-    169888,
-    169889,
-    169890, // Locked Extensive Strongbox
-    169891, // Locked Spellbound Strongbox
-    139113, // 행운의 상자 (kTERA)
-    166718, // 행운의 상자 (kTERA)
-    213026, // 행운의 상자 (kTERA)
+    139113, 166718, 213026, // 행운의 상자 (K TERA)
+    169886, 169887, 169888, 169889, 169890, 169891, // Locked ???? Strongbox
+    180668 // Fashion Coupon
 ]
 
 module.exports = function AutoLoot(d) {
